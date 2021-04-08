645e16ca5bed22273b2038cd5d29a1a60c2819fe
foday <foday_s@yahoo.com>
2021-04-08 15:51:09 -0500

    models, migrations, and null=True in models.py
    Setting the values to null of the CharField and DateTimeField will allow us to make changes to our database and if we ever import date and we don't have a name we are not going to get any, if we don't have null set to true we are going to get an error, a default value needs to be set

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -119,6 +119,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-#STATICFILES_DIRS = [
-#   os.path.join(BASE_DIR, 'static'),
-#]
+MEDIA_URL= '/images/'
+
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'static'),
+]
